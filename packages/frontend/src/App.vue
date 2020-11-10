a3ddcc33d593236918abf8f5c74d3a0c998b7131
Adnan Ali <adnan.ali@gmail.com>
2020-11-10 00:51:40 -0500

    Adding a input form for the link.

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -1,50 +1,13 @@
 <template>
-  <div class="font-sans">
-    <nav class="bg-gray-800">
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex items-center justify-between h-16">
-          <div class="flex items-center">
-            <div class="flex-shrink-0">
-              <span class="font-logo text-3xl text-blue-100">Reminisce</span>
-            </div>
-            <div class="hidden md:block">
-              <div class="ml-10 flex items-baseline space-x-4">
-                <a
-                  href="#"
-                  class="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700"
-                >Bookmarks</a>
-
-                <a
-                  href="#"
-                  class="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-                >Settings</a>
-              </div>
-            </div>
-          </div>
-          <div class="hidden md:block">
-            <div class="ml-4 flex items-center md:ml-6"></div>
-          </div>
-        </div>
-      </div>
-    </nav>
-
-    <header class="bg-white shadow-sm">
-      <div class="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
-        <h1 class="text-lg leading-6 font-semibold text-gray-900">Bookmarks</h1>
-      </div>
-    </header>
-    <main>
-      <div class="max-w-7xl mx-auto py-3 sm:px-6 lg:px-8">
-        <div class="px-4 py-6 sm:px-0">
-          Enter stuff here.
-        </div>
-      </div>
-    </main>
-  </div>
+  <Layout>
+    <BookmarkInput />
+  </Layout>
 </template>
 
 <script>
 import { ref } from "vue";
+import BookmarkInput from "./components/BookmarkInput.vue";
+import Layout from "./components/Layout.vue";
 
 const name = "World";
 const count = ref(0);
@@ -56,6 +19,8 @@ export default {
       count,
       add,
       name,
+      BookmarkInput,
+      Layout,
     };
   },
 };
---
+++ packages/frontend/src/components/BookmarkInput.vue
@@ -0,0 +1,52 @@
+<template>
+  <form @submit.prevent="submit">
+    <div class="max-w-3xl mx-auto">
+      <div class="mt-1 flex rounded-md shadow-sm">
+        <div class="relative flex items-stretch flex-grow focus-within:z-10">
+          <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+            <svg
+              class="h-5 w-5 text-gray-400"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </div>
+          <input
+            ref="link"
+            id="link"
+            class="form-input block w-full rounded-none rounded-l-md pl-10 transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+            placeholder="http link"
+          />
+        </div>
+        <button
+          class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-r-md text-gray-100 bg-blue-500 hover:text-gray-500 hover:bg-white focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
+        >Save</button>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const link = ref(null);
+    const submit = () => {
+      console.log(link.value.value);
+      setTimeout(() => link.value.value = "", 500)
+    };
+
+    return {
+      link,
+      submit,
+    };
+  },
+};
+</script>
